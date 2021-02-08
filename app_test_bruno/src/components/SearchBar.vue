f357a740936bca90e04cb8a8e37fd9f375db58df
buruno <hi@buruno.com.br>
2021-02-08 09:56:40 -0300

    refining search bar

--- app_test_bruno/src/components/SearchBar.vue
+++ app_test_bruno/src/components/SearchBar.vue
@@ -1,15 +1,16 @@
 <template>
   <main class="search-bar sticky w-12 shadow">
     <div class="w-12 container">
+      <i
+        v-if="searchTerm.length > 0" @click="searchTerm = ''"
+        class="icon icss-x text-md"
+      />
       <input
         class="w-12"
         type="text"
         v-model="searchTerm"
       />
-      <span class="icons text-color-2 text-md">
-        <i v-if="searchTerm.length > 0" @click="searchTerm = ''" class="icss-x" />&ensp;
-        <i class="icss-search" />
-      </span>
+      <i class="icon text-md icss-search" />
     </div>
   </main>
 </template>
@@ -45,22 +46,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/style/_variables';
+
 .container {
   position: relative;
   z-index: 20;
-
 }
 .search-bar {
   top: 5rem;
   background-color: white;
   input {
     width: 100%;
+    padding-left: 2rem;
     background-color: white;
   }
-  .icons {
+  .icon {
     position: absolute;
-    margin-top: 0.5rem;
-    right: 1.8rem;
+    margin-top: 0.8rem;
+    &.icss-x {
+      margin-left: 0.4rem;
+      cursor: pointer;
+      color: $color-1;
+    }
+    &.icss-search {
+      color: $color-2;
+      right: 1.8rem;
+    }
   }
 }
 </style>
